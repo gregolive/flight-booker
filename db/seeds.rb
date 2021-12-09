288002cd62e4a96c87bed227530ffc7868654897
Gregory Olive <gregoryolive@outlook.com>
2021-12-10 01:23:34 +0900

    Update db seed for Flight model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Airport Model
 
-airports = ['NRT', 'Tokyo'],
-           ['PKX', 'Beijing'],
-           ['JFK', 'New York'],
-           ['LAX', 'Los Angeles'],
-           ['YYZ', 'Toronto'],
-           ['LHR', 'London'],
-           ['CDG', 'Paris'],
-           ['SYD', 'Sydney'],
-           ['ICN', 'Seoul']
+airport_list = ['NRT', 'Tokyo'],
+               ['PKX', 'Beijing'],
+               ['JFK', 'New York'],
+               ['LAX', 'Los Angeles'],
+               ['YYZ', 'Toronto'],
+               ['LHR', 'London'],
+               ['CDG', 'Paris'],
+               ['SYD', 'Sydney'],
+               ['ICN', 'Seoul']
 
-airports.each do |code, city|
+airport_list.each do |code, city|
   Airport.create(code: code, city: city)
 end
 
-flights = [
-  {
-    departure_time: DateTime.strptime("12/12/2021 17:00", "%m/%d/%Y %H:%M"),
-    flight_duration: 200,
-    departure_airport_id: 10,
-    arrival_airport_id: 14
-  }
-]
+# Flight Model
 
-flights.each do |flight|
-  Flight.create(flight)
+dates = (Date.today..(Date.today + 1.month)).to_a
+
+times = []
+Date.today.step(Date.today + 1, 1.0 / (24 * 60)) { |t| times.push(t) }
+
+airports = [4, 5, 6, 7, 8, 9, 10, 13, 14]
+
+airports.each do |depart|
+  airports.each do |arrive|
+    next if depart == arrive
+
+    length = rand(90..300)
+    dates.each do |d|
+      rand(1..3).times do
+        t = times[rand(times.length)].to_datetime
+        Flight.create!(
+          departure_time: DateTime.new(d.year, d.month, d.day, t.hour, t.min, t.sec, t.zone),
+          flight_duration: length,
+          departure_airport_id: depart,
+          arrival_airport_id: arrive
+        )
+      end
+    end
+  end
 end
