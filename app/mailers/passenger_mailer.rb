d79b69cc67ac1a04bd56f53cbcc981437bee41ea
Gregory Olive <gregoryolive@outlook.com>
2022-01-03 23:17:38 +0900

    Add mailer views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def welcome_email
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: 'Flight Confirmation with bookr')
+  end
 end
